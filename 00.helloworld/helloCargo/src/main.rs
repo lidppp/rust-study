b16c9e07c753cfe4cabad1645ed96e6b5007ab2f
lidppp <1512282028@qq.com>
2023-01-12 16:53:34 +0800

    添加了svgdemo,canvas渲染svgdemo

--- 00.helloworld/helloCargo/src/main.rs
+++ 00.helloworld/helloCargo/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     println!("Hello, world!");
     println!("this is my first application for rust-cargo!");
+
+    let s = 2;
+    
+    print!("{}\n",s);
+    print!("{}\n",s);
+    print!("{}\n",s);
 }
